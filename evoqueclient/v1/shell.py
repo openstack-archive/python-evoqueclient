d1ec14c77fbd85174e751e8432672ede995b237e
sin <xunamingyi@forpikus.com>
2015-11-10 16:49:13 +0800

    Create Workflow Command
    Can use `evoque workflow-create -f workflow.yaml`.
    
    Change-Id: I8726e5661fb2a206c3e67b18856dcb1878e5a5f1

--- evoqueclient/v1/shell.py
+++ evoqueclient/v1/shell.py
@@ -9,8 +9,10 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import os
 
 from evoqueclient.common import utils
+from evoqueclient.openstack.common.apiclient import exceptions
 
 
 def do_ticket_list(ec, args={}):
@@ -26,3 +28,15 @@ def do_ticket_list(ec, args={}):
 def do_ticket_create(ec, args):
     """Create a ticket."""
     ec.tickets.add({"name": args.name})
+
+
+@utils.arg("-f", "--file-name", metavar="<FILE-NAME>",
+           help="Workflow file.")
+def do_workflow_create(ec, args):
+    """Create a workflow."""
+    if not args.file_name:
+        raise exceptions.CommandError(
+            "Provide --file-name <FILE-NAME> for workflow "
+            "creation.")
+    file_name = os.path.abspath(os.path.expanduser(args.file_name))
+    ec.workflows.add({"file": file_name})
